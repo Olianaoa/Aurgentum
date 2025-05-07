443fbe2442c85a50d02f78ccea925927040c641b
Оляна Владимирова <olianavladimirova@gmail.com>
2025-05-07 19:21:20 +0300

    6 commit

--- src/blocks/catalog_for_main.vue
+++ src/blocks/catalog_for_main.vue
@@ -7,13 +7,13 @@ import { RouterLink, RouterView } from 'vue-router';
     <div class="first_line line">
         <RouterLink :to="{ name: 'catalog' }">
             <div class="block background"
-                style="background-image: url(https://github.com/Olianaoa/Aurgentum/blob/main/src/img/catalog_img/%D0%BA%D0%BE%D0%BB%D1%8C%D1%86%D0%BE_%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3.png?raw=true);">
+                style="background-image: url(https://raw.githubusercontent.com/Olianaoa/Aurgentum/refs/heads/main/src/assets/catalog_img/%D0%BA%D0%BE%D0%BB%D1%8C%D1%86%D0%BE_%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3.png);">
                 <p class="underline">Кольца</p>
             </div>
         </RouterLink>
         <RouterLink :to="{ name: 'catalog' }">
             <div class="block background"
-                style="background-image: url(https://github.com/Olianaoa/Aurgentum/blob/main/src/img/catalog_img/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3_%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82.png?raw=true);">
+                style="background-image: url(https://raw.githubusercontent.com/Olianaoa/Aurgentum/refs/heads/main/src/assets/catalog_img/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3_%D0%B1%D1%80%D0%B0%D1%81%D0%BB%D0%B5%D1%82.png);">
                 <p class="underline">Браслеты</p>
             </div>
         </RouterLink>
@@ -25,19 +25,19 @@ import { RouterLink, RouterView } from 'vue-router';
         <p>Твоя история начинается с блеска</p>
         <RouterLink :to="{ name: 'catalog' }">
             <div class="block background"
-                style="background-image: url(https://github.com/Olianaoa/Aurgentum/blob/main/src/img/catalog_img/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3_%D1%81%D0%B5%D1%80%D1%8C%D0%B3%D0%B8.png?raw=true);">
+                style="background-image: url(https://raw.githubusercontent.com/Olianaoa/Aurgentum/refs/heads/main/src/assets/catalog_img/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3_%D1%81%D0%B5%D1%80%D1%8C%D0%B3%D0%B8.png);">
                 <p class="underline">Серьги</p>
             </div>
         </RouterLink>
         <RouterLink :to="{ name: 'catalog' }">
             <div class="block background"
-                style="background-image: url(https://github.com/Olianaoa/Aurgentum/blob/main/src/img/catalog_img/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3_%D0%BA%D0%BE%D0%BB%D1%8C%D0%B5_%D0%BA%D1%83%D0%BB%D0%BE%D0%BD.png?raw=true);">
+                style="background-image: url(https://raw.githubusercontent.com/Olianaoa/Aurgentum/refs/heads/main/src/assets/catalog_img/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3_%D0%BA%D0%BE%D0%BB%D1%8C%D0%B5_%D0%BA%D1%83%D0%BB%D0%BE%D0%BD.png);">
                 <p class="underline">Колье и кулоны</p>
             </div>
         </RouterLink>
         <RouterLink :to="{ name: 'catalog' }">
             <div class="block background" 
-            style="background-image: url(https://github.com/Olianaoa/Aurgentum/blob/main/src/img/catalog_img/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3_%D0%B2%D1%81%D0%B5_%D1%83%D0%BA%D1%80.png?raw=true); 
+            style="background-image: url(https://raw.githubusercontent.com/Olianaoa/Aurgentum/refs/heads/main/src/assets/catalog_img/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3_%D0%B2%D1%81%D0%B5_%D1%83%D0%BA%D1%80.png); 
             color:var(--white)">
                 <p class="underline">Все украшения ></p>
             </div>
